6a39ad8b1f18d256013b53cb8c843ee64de4d0fd
Georges Cosson <17021662+stressGC@users.noreply.github.com>
2022-12-03 10:24:17 +0100

    feat: setup dev env

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+	// Indicates whether the coverage information should be collected while executing the test
+	collectCoverage: false,
+
+	// An array of file extensions your modules use
+	moduleFileExtensions: ["ts"],
+
+	// A map from regular expressions to paths to transformers
+	transform: {
+		"^.+\\.tsx?$": "babel-jest",
+	},
+}
